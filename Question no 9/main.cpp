721d0229cd81b9e60a09779ba5526e26843d7509
Utpal Kant <82315931+Utpalkant6204@users.noreply.github.com>
2022-05-06 16:49:19 +0530

    Create main.cpp

---
+++ Question no 9/main.cpp
@@ -0,0 +1,17 @@
+// buy and sell stocks II
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+      int ans=0,x = prices[0],p;
+      int n = prices.size();
+      for(int i=0;i<n;i++)
+      {
+       x = min(x,prices[i]);
+        p = prices[i]-x;
+        ans = max(ans,p);
+        
+      }
+      return ans;
+    }
+};
